0267330c2af1bb7b94c165337a29d09338d76a69
f3an <chenyavskiy2019ios@gmail.com>
2023-09-25 02:49:56 +0300

    created auth middleware

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,16 @@
+const auth = async (req, reply) => {
+    try {
+        const token = req.headers.authorization;
+        const API_KEY = process.env.API_KEY;
+
+        if (!token || token !== API_KEY ) {
+            return reply.code(409).send({ error: 'Unauthorized' });
+        };
+
+    } catch (error) {
+        console.error('Error in auth middleware:', error);
+        reply.code(500).send({ message: 'Internal Server Error' });   
+    }
+};
+
+module.exports = auth;
