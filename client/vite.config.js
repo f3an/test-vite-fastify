c7209c6a833bbe4b7092d90af37600559d14014e
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2023-10-14 18:01:58 +0300

    added new features to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: process.env.VITE_PORT || 3001,
+  },
+  resolve: {
+    alias: {
+      '@components': '/src/app/components',
+      '@helpers': '/src/app/helpers',
+      '@services': '/src/app/services',
+      '@routes': '/src/app/routes',
+      '@hooks': '/src/app/hooks',
+      '@pages': '/src/app/pages',
+      '@redux': '/src/app/redux',
+      '@assets': '/src/assets',
+      '@app': '/src/app',
+      '@': '/src',
+    },
+  },
 })
